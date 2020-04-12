891d1a2d16b321e872719611247b26014b355f57
Marshmellowon <leo503801@gmail.com>
2020-04-12 13:37:36 +0900

    edit

--- Main.java
+++ Main.java
@@ -8,16 +8,22 @@ public class Main {
         int n, m;
         int left = 0;
         int right = 0;
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int i[] = new int[n];
+        n = sc.nextInt(); /* 날짜 */
+        m = sc.nextInt(); /* m번 */
+        int i[] = new int[n]; /* 금액 */
 
+        System.out.println(n + "일동안 사용");
         for (int j = 0; j < n; j++) {
             i[j] = sc.nextInt();
+            System.out.println(j + 1 + "번째 날:" + i[j]);
+
             left = i[j] > left ? i[j] : left;
+            System.out.println("금액: " + left);
+            /* 필요금액 총합 */
             right += i[j];
         }
 
+        /* 이진탐색 알고리즘 */
         while (left <= right) {
             int count = 0;
             int sum = 0;
